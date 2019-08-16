a538d342a6e0ecc741b0b9af2ca183326d336cb8
wuwei <540165253@qq.com>
2019-08-16 15:19:04 +0800

    Fourth Commit

--- index-config-server/src/main/java/com/ww/trend/IndexConfigServerApplication.java
+++ index-config-server/src/main/java/com/ww/trend/IndexConfigServerApplication.java
@@ -1,18 +1,18 @@
 package com.ww.trend;
-
+  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+  
 import cn.hutool.core.util.NetUtil;
-
+  
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class IndexConfigServerApplication {
 	
     public static void main(String[] args) {
         int port = 8060;
@@ -26,7 +26,7 @@ public class ConfigServerApplication {
             System.err.printf("端口%d被占用了，无法启动%n", port );
             System.exit(1);
         }
-        new SpringApplicationBuilder(ConfigServerApplication.class).properties("server.port=" + port).run(args);
+        new SpringApplicationBuilder(IndexConfigServerApplication.class).properties("server.port=" + port).run(args);
     }
     
 }
