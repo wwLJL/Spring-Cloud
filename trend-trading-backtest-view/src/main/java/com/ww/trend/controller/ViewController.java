78d6cc9925ea68bcab4a7e91e5cfadf053ea064f
wuwei <540165253@qq.com>
2019-08-19 16:41:30 +0800

    ninth commit

--- trend-trading-backtest-view/src/main/java/com/ww/trend/controller/ViewController.java
+++ trend-trading-backtest-view/src/main/java/com/ww/trend/controller/ViewController.java
@@ -1,14 +1,10 @@
 package com.ww.trend.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-	
-	@Value("${version}")
-    String version;
 	
 	@GetMapping("/")
     public String view() {
